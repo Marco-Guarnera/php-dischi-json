fe6f345cd6990dffb3cf195aa0e5ff544f5b1f4b
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-08 15:26:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Fase di preparazione
+
+const app = Vue.createApp({
+    name: "PHP JSON",
+    data: () => ({}),
+    methods: {}
+});
+
+app.mount("#app");
+
+// Variabili
+// Strutture dati
+// Funzioni
+
+// Fase di raccolta dati
+// Fase di elaborazione
+// Fase di produzione
---
+++ server/server.php
@@ -0,0 +1,15 @@
+<?php
+
+// Fase di preparazione
+
+// Variabili
+// Strutture dati
+// Funzioni
+
+// Fase di raccolta dati
+// Fase di elaborazione
+// Fase di produzione
+
+echo 'Test';
+
+?>
